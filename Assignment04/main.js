403d8d57be2879e8a969f5225f001433cce73418
Piyush Yadav <piyushcoolyadav1997@gmail.com>
2018-05-24 10:02:24 +0530

    fixes

--- Assignment04/main.js
+++ Assignment04/main.js
@@ -1,7 +1,7 @@
 var imgPrompt = function () {
 	var caption = prompt("Enter Caption");
 	var imgAddr = prompt("Enter image address");
-	if (imgAddr !== "" && caption!=="") {
+	if (imgAddr !== "") {
 		var ic = $("#imageCard").clone(true);
 		$("#imageLink").attr("src",imgAddr);
 		$("#caption").text(caption);
